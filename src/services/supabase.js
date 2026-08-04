3e54264a67ed7c020fd72e9b5311aea86f7790b9
osorniou62-hue <osorniou62@gmail.com>
2026-08-04 13:10:54 -0600

    a3

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -7,7 +7,7 @@ const supabaseUrl =
 
 const supabaseAnonKey =
   import.meta.env
-    .VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxia3BzdWpoYW95ZGhqeG50amRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NTA0MjIsImV4cCI6MjEwMTQyNjQyMn0.dybAVHxcD_JT-XlR2hnRDSyQmr9nntQeKKRn3IarseM;
+    .VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxia3BzdWpoYW95ZGhqeG50amRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NTA0MjIsImV4cCI6MjEwMTQyNjQyMn0.dybAVHxcD_JT-XlR2hnRDSyQmr9nntQeKKRn3IarseM
 
 export const supabase =
   createClient(
